be4eaa8d2ee56d0c3f671bfa5408a4d57440b84a
schrammbox <schrammbox@gmail.com>
2022-01-03 23:45:54 +0100

    - Created QuestionnaireRepository - Created QuestionnaireService according to UML_DIA.drawio - Created Questionnaire Entity + Enum (OrderType)

---
+++ src/main/java/com/itconsult/itconsult/entity/QuestionaireEnums/OrderType.java
@@ -0,0 +1,13 @@
+package com.itconsult.itconsult.entity.QuestionaireEnums;
+
+public enum OrderType {
+    SECURITY_ARCHITECTURE,
+    DATABASE,
+    URGENT_SECURITY_PROBLEM,
+    HARDWARE,
+    SCALABILITY,
+    CLOUD,
+    INFRASTRUCTURE,
+    SOFTWARE_DEVELOPMENT,
+    ADMINISTRATION
+}
