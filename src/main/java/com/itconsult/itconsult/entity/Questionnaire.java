cf15cfb9f285392f6944ac5f846d67662dce8390
schrammbox <schrammbox@gmail.com>
2022-01-03 23:45:54 +0100

    - Created QuestionnaireRepository - Created QuestionnaireService according to UML_DIA.drawio - Created Questionnaire Entity + Enum (OrderType)

--- src/main/java/com/itconsult/itconsult/entity/Questionnaire.java
+++ src/main/java/com/itconsult/itconsult/entity/Questionnaire.java
@@ -1,13 +1,29 @@
 package com.itconsult.itconsult.entity;
 
+import com.itconsult.itconsult.entity.QuestionaireEnums.OrderType;
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity(name = "")
 public class Questionnaire {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private String urgency;
+    private String duration;
+    private String CompanyDescription;
+    private String ProblemDescription;
+    private OrderType orderType;
+
 }
--- src/main/java/com/itconsult/itconsult/service/QuestionnaireService.java
+++ src/main/java/com/itconsult/itconsult/service/QuestionnaireService.java
@@ -1,5 +1,7 @@
 package com.itconsult.itconsult.service;
 
+import com.itconsult.itconsult.entity.QuestionaireEnums.OrderType;
+import com.itconsult.itconsult.entity.Questionnaire;
 import com.itconsult.itconsult.repository.QuestionnaireRepository;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,6 +12,25 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class QuestionnaireService {
     private final QuestionnaireRepository questionnaireRepository;
+    private OrderService orderService;
 
+    /**
+     * Frage:
+     * Macht der @return hier Sinn?
+     */
+    public Questionnaire getEmptyQuestionaire() {
+        return new Questionnaire();
+    }
 
+    public Questionnaire createOrderFromQuestionaire(String urgency, String duration, String CompanyDescription,
+                                                     String ProblemDescription, OrderType orderType) {
+        return questionnaireRepository.save(Questionnaire.builder()
+                .urgency(urgency)
+                .duration(duration)
+                .CompanyDescription(CompanyDescription)
+                .ProblemDescription(ProblemDescription)
+                .orderType(orderType)
+                .build());
+
+    }
 }
