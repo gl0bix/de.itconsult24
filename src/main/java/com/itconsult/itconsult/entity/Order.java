6fb2c86dbebd5775ad5ce43397dc6dc0b89b4f20
LiterallyJan <58440059+LiterallyJan@users.noreply.github.com>
2022-01-09 18:20:20 +0100

    -Added Entity name for Order class, to solve CommandAcceptanceException for DDL

--- src/main/java/com/itconsult/itconsult/entity/Order.java
+++ src/main/java/com/itconsult/itconsult/entity/Order.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity(name = "")
+@Entity(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
