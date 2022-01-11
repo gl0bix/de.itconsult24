cf3b5f46b01663cdf343ee2c4748dc65240d4cfe
LiterallyJan <58440059+LiterallyJan@users.noreply.github.com>
2022-01-11 11:30:34 +0100

    -Manually added imports to Order

--- src/main/java/com/itconsult/itconsult/entity/Order.java
+++ src/main/java/com/itconsult/itconsult/entity/Order.java
@@ -5,6 +5,8 @@ import lombok.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 import javax.persistence.Id;
 import java.util.Date;
 
